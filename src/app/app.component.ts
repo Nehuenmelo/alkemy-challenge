72fa476c98cbd35c563021e4a060ec3ac99b1714
Nehuén Melo <37715466+Nehuenmelo@users.noreply.github.com>
2020-12-02 15:06:51 -0300

    Update app.component.ts
    edit title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Alkemy';
 
-  public inicio = "Ejericio para Alkemy";
+  public inicio = "Ejercicio para Alkemy";
 }
